ee8c0c9c50028ccb2c64255a0df59d748b5765a7
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-05-21 22:56:11 +0100

    add background and car

--- commtransport/routes.py
+++ commtransport/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import flash, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from commtransport import app, db
 from commtransport.models import User, Place, Approval, Request
 
@@ -6,3 +7,78 @@ from commtransport.models import User, Place, Approval, Request
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = User.query.filter(User.user_name ==
+                                           request.form.get("username").lower()).all()
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        user = User(
+            user_name=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = User.query.filter(User.user_name ==
+                                           request.form.get("username").lower()).all()
+
+        if existing_user:
+            print(request.form.get("username"))
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user[0].password, request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profilePage", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("signin"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("signin"))
+
+    return render_template("signin.html")
+
+
+@app.route("/profile-page/<username>", methods=["GET", "POST"])
+def profilePage(username):
+    # grab the session user's username from db
+    if "user" in session:
+        return render_template("profile.html", username=session["user"])
+
+    return redirect(url_for("login"))
+
+
+@app.route("/signout")
+def signout():
+    # remove user from session cookies
+    flash("You have been signed out.")
+    # session.clear() would clear all cookies related to our app
+    session.pop("user")
+    return redirect(url_for("signin"))
--- commtransport/static/js/script.js
+++ commtransport/static/js/script.js
@@ -85,3 +85,97 @@ $(document).ready(function () {
 			});
 	}
 });
+
+// google maps autocompletion version 1 
+
+    function initMap() {
+      const CONFIGURATION = {
+        "ctaTitle": "Enter",
+        "mapOptions": {"center":{"lat":37.4221,"lng":-122.0841},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":true,"zoom":11,"zoomControl":true,"maxZoom":22,"mapId":""},
+        "mapsApiKey": "AIzaSyCJKHrw61FGKpNiIY4H5eZhJF2rEYn1DJ0",
+        "capabilities": {"addressAutocompleteControl":true,"mapDisplayControl":false,"ctaControl":true}
+      };
+      const componentForm = [
+        'location',
+        'locality',
+        'administrative_area_level_1',
+        'country',
+        'postal_code',
+      ];
+
+      const getFormInputElement = (component) => document.getElementById(component + '-input');
+      const autocompleteInput = getFormInputElement('location');
+      const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
+        fields: ["address_components", "geometry", "name"],
+        types: ["address"],
+      });
+      autocomplete.addListener('place_changed', function () {
+        const place = autocomplete.getPlace();
+        if (!place.geometry) {
+          // User entered the name of a Place that was not suggested and
+          // pressed the Enter key, or the Place Details request failed.
+          window.alert('No details available for input: \'' + place.name + '\'');
+          return;
+        }
+        fillInAddress(place);
+      });
+
+      function fillInAddress(place) {  // optional parameter
+        const addressNameFormat = {
+          'street_number': 'short_name',
+          'route': 'long_name',
+          'locality': 'long_name',
+          'administrative_area_level_1': 'short_name',
+          'country': 'long_name',
+          'postal_code': 'short_name',
+        };
+        const getAddressComp = function (type) {
+          for (const component of place.address_components) {
+            if (component.types[0] === type) {
+              return component[addressNameFormat[type]];
+            }
+          }
+          return '';
+        };
+        getFormInputElement('location').value = getAddressComp('street_number') + ' '
+                  + getAddressComp('route') + ' ' + getAddressComp('locality') + ' '
+				  + getAddressComp('administrative_area_level_1') + ' '
+				 + getAddressComp('postal_code');
+        for (const component of componentForm) {
+          // Location field is handled separately above as it has different logic.
+          if (component !== 'location') {
+            getFormInputElement(component).value = getAddressComp(component);
+          }
+        }
+      }
+    }
+  
+// google map autocomplete version 2 
+
+// const addressInput = document.getElementById('address-input');
+// // Specify just the place data fields that you need.
+// const autocomplete = new google.maps.places.Autocomplete(addressInput, {
+// 	fields: ['place_id', 'geometry', 'formatted_address', 'name'],
+// });
+
+// autocomplete.addListener("place_changed", () => {
+
+//     const place = autocomplete.getPlace();
+
+//     if (!place.geometry || !place.geometry.location) {
+//       return;
+//     }
+
+
+//     // Set the position of the marker using the place ID and location.
+//     // @ts-ignore This should be in @typings/googlemaps.
+//     addressInput.value = {
+//       placeId: place.place_id,
+//       location: place.geometry.location,
+//     };
+
+// 	console.log(addressInput)
+    
+//   });
+
+//   window.initMap = initMap;
