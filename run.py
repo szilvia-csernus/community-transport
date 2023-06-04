6a729bcd4393dc5235d0a9b3349ee72e4ca7ddf2
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-06-05 00:13:09 +0100

    edit run.py file

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from commtransport import app
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get(""),
+        host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
